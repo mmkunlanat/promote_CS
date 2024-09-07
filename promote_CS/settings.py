dd40fecf32436d3d42b8fed4c01bfe6b860da64a
Sarinya47 <Sarinya47@gmail.com>
2024-09-07 16:46:44 +0700

    config settings

--- promote_CS/settings.py
+++ promote_CS/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = "promote_CS.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
