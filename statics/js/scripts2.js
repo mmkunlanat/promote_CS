5b6b33ab58946127b70541ed8d26aeb24c1441fa
mmkunlanat <stu6612732138@sskru.ac.th>
2024-09-08 19:19:24 +0700

    add bg-video

--- statics/js/scripts2.js
+++ statics/js/scripts2.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Small Business v5.0.6 (https://startbootstrap.com/template/small-business)
+* Start Bootstrap - Coming Soon v6.0.7 (https://startbootstrap.com/theme/coming-soon)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
